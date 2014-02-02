9207326714aee8c5aece009031f1e8c7fc124844
Benjamin Roth <apnea.diving.deep@gmail.com>
2014-02-02 18:14:39 +0100

    multi markers

--- js/examples.js
+++ js/examples.js
@@ -224,7 +224,21 @@ angular.module('App').service('App.MapBuilders', function(){
         function(){ }
       );
 
-    }
+    },
+    multi_markers: function(){
+      var handler = Gmaps.build('Google');
+      handler.buildMap({ internal: {id: 'multi_markers'}}, function(){
+        var markers = handler.addMarkers([
+          { lat: 43, lng: 3.5},
+          { lat: 45, lng: 4},
+          { lat: 47, lng: 3.5},
+          { lat: 49, lng: 4},
+          { lat: 51, lng: 3.5}
+        ]);
+        handler.bounds.extendWith(markers);
+        handler.fitMapToBounds();
+      });
+    },
 
   };
 })
